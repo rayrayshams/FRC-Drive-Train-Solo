2851c8ebde711101ead959e6e321c0a77d0501b9
AniCricKet <91163802+AniCricKet@users.noreply.github.com>
2021-11-02 16:24:46 -0700

    Update RobotContainer.java

--- FRC Drive Train SOLO/src/main/java/frc/robot/RobotContainer.java
+++ FRC Drive Train SOLO/src/main/java/frc/robot/RobotContainer.java
@@ -3,46 +3,58 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+
 import edu.wpi.first.wpilibj.XboxController.Axis;
+import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import frc.robot.commands.ArcadeDrive;
+import frc.robot.commands.Shoot;
 import frc.robot.commands.Trigger;
 import frc.robot.subsystems.DriveTrainSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and button mappings) should be declared here.
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a "declarative" paradigm, very little robot logic should
+ * actually be handled in the {@link Robot} periodic methods (other than the
+ * scheduler calls). Instead, the structure of the robot (including subsystems,
+ * commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   public final DriveTrainSubsystem m_drive = new DriveTrainSubsystem();
+  public final ShooterSubsystem m_shooter = new ShooterSubsystem();
 
   public XboxController m_controller = new XboxController(0);
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
-    m_drive.setDefaultCommand(
-      new ArcadeDrive(m_drive, () -> m_controller.getY(Hand.kLeft), () -> m_controller.getX(Hand.kRight), () ->m_controller.getTriggerAxis(Hand.kLeft))
+    m_drive.setDefaultCommand(new ArcadeDrive(m_drive, () -> m_controller.getY(Hand.kLeft),
+        () -> m_controller.getX(Hand.kRight), () -> m_controller.getTriggerAxis(Hand.kLeft))
 
-      );
+    );
+
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by instantiating a {@link GenericHID} or one of its subclasses
+   * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then
+   * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    new Trigger(() ->m_controller.getTriggerAxis(Hand.kLeft));
-
+    new Trigger(() -> m_controller.getTriggerAxis(Hand.kLeft));
+    new JoystickButton(m_controller, Button.kA.value).whenPressed(new Shoot(m_shooter))
+        .whenReleased(new ShootStop(m_shooter));
   }
 
   /**
