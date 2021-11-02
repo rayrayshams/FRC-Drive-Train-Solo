63d185be53c71aadef91de4945378451f1af09f9
Colin Mills <colin.lego.scout@gmail.com>
2021-11-02 15:51:42 -0700

    commit

--- FRC Drive Train SOLO/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ FRC Drive Train SOLO/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -4,13 +4,14 @@
 
 package frc.robot.subsystems;
 
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.revrobotics.CANEncoder;
@@ -33,20 +34,22 @@ public class Shooter {
   private Joystick m_stick;
   private static final int deviceID = 1;
   private CANSparkMax m_motor;
-  private CANPIDController m_pidController;
+  private CANPIDController m_controller;
+  private PIDController m_pidController;
   private CANEncoder m_encoder;
   public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM;
 
 public Shooter(){
-  m_pidController.setP(kP);
-  m_pidController.setI(kI);
-  m_pidController.setD(kD);
-  m_pidController.setIZone(kIz);
-  m_pidController.setFF(kFF);
-  m_pidController.setOutputRange(kMinOutput, kMaxOutput);
-  
+  m_motor = new CANSparkMax(10, MotorType.kBrushless);
+  m_controller.setP(kP);
+  m_controller.setI(kI);
+  m_controller.setD(kD);
+  m_controller.setIZone(kIz);
+  m_controller.setFF(kFF);
+  m_controller.setOutputRange(kMinOutput, kMaxOutput);
+  m_pidController = new PIDController(Constants.Shooter.kP, Constants.Shooter.kI, Constants.Shooter.kD);
   double setPoint = m_stick.getY()*maxRPM;
-  m_pidController.setReference(setPoint, ControlType.kVelocity);
+  m_controller.setReference(setPoint, ControlType.kVelocity);
   SmartDashboard.putNumber("SetPoint", setPoint);
   SmartDashboard.putNumber("ProcessVariable", m_encoder.getVelocity());
 }
